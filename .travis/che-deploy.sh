c7a87f3e8dcaf7aa8c7428305dc9faccb3b66993
Siddhesh Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2021-05-11 11:49:55 +0530

    Update che-deploy.sh

--- .travis/che-deploy.sh
+++ .travis/che-deploy.sh
@@ -30,3 +30,25 @@ if [[ $channel !== 'next' && $channel !== 'stable' ]]; then
 fi
 echo "Installing chectl [channel=${channel}]...";
 $CHECTL_SCRIPT_PATH --channel=${channel}
+
+#https://github.com/che-incubator/che-deploy-action/blob/main/src/che-helper.ts
+echo "Eclipse Che [serverDeploy]..."
+echo "Calling chectl server:deploy..."
+DEFAULT_CONFIG='{
+  "spec":{
+    "auth":{
+      "updateAdminPassword":false
+    },
+    "server":{
+      "customCheProperties":{
+        "CHE_WORKSPACE_SIDECAR_IMAGE__PULL__POLICY":"IfNotPresent",
+        "CHE_WORKSPACE_PLUGIN__BROKER_PULL__POLICY":"IfNotPresent",
+        "CHE_INFRA_KUBERNETES_PVC_JOBS_IMAGE_PULL__POLICY":"IfNotPresent"
+      }
+    }
+  }
+}' 
+
+#TODO: https://github.com/che-incubator/che-deploy-action/blob/main/src/che-helper.ts#L43-L70
+local customResourceContent=
+CUSTOM_RESOURCE_PATH="/tmp/custom-resource-patch.yaml"
